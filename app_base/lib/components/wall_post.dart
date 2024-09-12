4886fbd0724e89c58388ffb099c0fc09ed513018
RyanP0612 <128430984+RyanP0612@users.noreply.github.com>
2024-09-11 22:18:03 -0300

    like commit
    like nos comentarios e ajustes

--- app_base/lib/components/wall_post.dart
+++ app_base/lib/components/wall_post.dart
@@ -1,19 +1,77 @@
+import 'package:app_base/components/like_button.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
-class LeticiaLindaPost extends StatelessWidget {
-final String message;
-final String user;
+class LeticiaLindaPost extends StatefulWidget {
+  final String message;
+  final String user;
+  final String postId;
+  final List<String> likes;
 
-
-  const LeticiaLindaPost({super.key, required this.message, required this.user});
+  const LeticiaLindaPost(
+      {super.key, required this.message, required this.user, required this.postId, required this.likes});
 
   @override
+  State<LeticiaLindaPost> createState() => _LeticiaLindaPostState();
+}
+
+class _LeticiaLindaPostState extends State<LeticiaLindaPost> {
+  // user
+  final currentUser = FirebaseAuth.instance.currentUser!;
+  bool isLiked = false;
+
+
+  @override
+
+  void initState(){
+    super.initState();
+    isLiked = widget.likes.contains(currentUser.email);
+    // resolve o erro em likes: List<String>.from(post['Likes'] ?? []),
+  }
+
+  void toggledLike(){
+    setState(() {
+      isLiked = !isLiked;
+    });
+  }
+
+    @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        Text(user),
-        Text(message)
-      ],
+    return Container(
+      decoration: BoxDecoration(
+          color: Colors.white, borderRadius: BorderRadius.circular(8)),
+      margin: EdgeInsets.only(top: 25, left: 25, right: 25),
+      padding: EdgeInsets.all(25),
+      child: Row(
+        children: [
+          Column(
+            children: [
+// like button
+              LikeButton(isLiked: isLiked, onTap:toggledLike),
+
+// like count
+            ],
+          ),
+
+          SizedBox(
+            width: 20,
+          ),
+          // mensagem e email do usuario
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                widget.user,
+                style: TextStyle(color: Colors.grey[500]),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Text(widget.message)
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
--- app_base/lib/pages/home_page.dart
+++ app_base/lib/pages/home_page.dart
@@ -6,7 +6,6 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
-
   const HomePage({super.key});
 
   @override
@@ -14,28 +13,34 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-
   // user
   final currentUser = FirebaseAuth.instance.currentUser!;
 
   // text controller
   final textController = TextEditingController();
 
-  void signOut(){
+  void signOut() {
     FirebaseAuth.instance.signOut();
   }
 
   // post message
 
-  void postMessage(){
+  void postMessage() {
 // só poste se tiver algo no textfield
-if(textController.text.isNotEmpty){
-  FirebaseFirestore.instance.collection("User Post").add({
-    "UserEmail": currentUser.email,
-    "Message":textController.text,
-    "TimeStamp": Timestamp.now(),
-  });
-}
+    if (textController.text.isNotEmpty) {
+      FirebaseFirestore.instance.collection("User Post").add({
+        "UserEmail": currentUser.email,
+        "Message": textController.text,
+        "TimeStamp": Timestamp.now(),
+        'Likes':[],
+      });
+    }
+
+    // limpar textField
+
+    setState(() {
+      textController.clear();
+    });
   }
 
   @override
@@ -45,69 +50,90 @@ if(textController.text.isNotEmpty){
       appBar: AppBar(
         backgroundColor: Colors.grey[900],
         centerTitle: true,
-        title: Text("Leticia lindaaa 123", style: TextStyle(color: Colors.grey[300], fontWeight: FontWeight.bold),
-        
+        title: Text(
+          "Leticia lindaaa 123",
+          style:
+              TextStyle(color: Colors.grey[300], fontWeight: FontWeight.bold),
         ),
-
         actions: [
-          IconButton(onPressed: signOut, icon: Icon(Icons.logout, color:Colors.grey[300],))
+          IconButton(
+              onPressed: signOut,
+              icon: Icon(
+                Icons.logout,
+                color: Colors.grey[300],
+              ))
         ],
       ),
       body: Center(
         child: Column(
           children: [
             // leticia linda 123
-        
-       Expanded(
-  child: StreamBuilder(
-    // Define o fluxo de dados que o StreamBuilder irá escutar
-    stream: FirebaseFirestore.instance
-        .collection("User Posts") // Acessa a coleção chamada "User Posts" no Firestore
-        .orderBy("Timestamp", descending: false) // Ordena os documentos pelo campo "Timestamp" em ordem crescente
-        .snapshots(), // Retorna um fluxo de atualizações em tempo real
-    builder: (context, snapshot) {
-      // Verifica se o snapshot contém dados
-      if (snapshot.hasData) {
-        return ListView.builder(
-          // tira o erro que da toda hora kkk
-          itemCount: snapshot.data!.docs.length,
-          // Cria uma lista de itens de forma eficiente
-          itemBuilder: (context, index) {
-            // Obtém o documento do índice atual da lista de documentos
-            final post = snapshot.data!.docs[index];
-            return LeticiaLindaPost(message: post['Message'], user: post["UserEmail"]);
-            // O retorno do widget para cada item da lista deve ser definido aqui
-          },
-        );
-      } else if(snapshot.hasError){
-        return Center(child: Text("Erro: ${snapshot.error.toString()}"),);
-      }
-      return const Center(child: CircularProgressIndicator(),);
-    },
-  ),
-),
 
-        
+            Expanded(
+              child: StreamBuilder(
+                // Define o fluxo de dados que o StreamBuilder irá escutar
+                stream: FirebaseFirestore.instance
+                    .collection(
+                        "User Post") // Acessa a coleção chamada "User Posts" no Firestore
+                    .orderBy("TimeStamp",
+                        descending:
+                            false) // Ordena os documentos pelo campo "Timestamp" em ordem crescente
+                    .snapshots(), // Retorna um fluxo de atualizações em tempo real
+                builder: (context, snapshot) {
+                  // Verifica se o snapshot contém dados
+                  if (snapshot.hasData) {
+                    return ListView.builder(
+                      // tira o erro que da toda hora kkk
+                      itemCount: snapshot.data!.docs.length,
+                      // Cria uma lista de itens de forma eficiente
+                      itemBuilder: (context, index) {
+                        // Obtém o documento do índice atual da lista de documentos
+                        final post = snapshot.data!.docs[index];
+                        return LeticiaLindaPost(
+                            message: post['Message'], user: post["UserEmail"], postId: post.id,likes: List<String>.from(post['Likes'] ?? []),);
+                        // O retorno do widget para cada item da lista deve ser definido aqui
+                      },
+                    );
+                  } else if (snapshot.hasError) {
+                    return Center(
+                      child: Text("Erro: ${snapshot.error.toString()}"),
+                    );
+                  }
+                  return const Center(
+                    child: CircularProgressIndicator(),
+                  );
+                },
+              ),
+            ),
+
             // post mensagem
             Padding(
               padding: const EdgeInsets.all(25.0),
               child: Row(
-              
                 children: [
                   // textfield
                   Expanded(
-                    child: MyTextField(controller: textController, hintText: "Escreva algo...", obscureText: false)
-                     ),
-              
-                    //  postbutton
-                    IconButton(onPressed: postMessage, icon: Icon(Icons.arrow_circle_up)),
-              ],
+                      child: MyTextField(
+                          controller: textController,
+                          hintText: "Escreva algo...",
+                          obscureText: false)),
+
+                  //  postbutton
+                  IconButton(
+                      onPressed: postMessage,
+                      icon: Icon(Icons.arrow_circle_up)),
+                ],
               ),
             ),
-        
-        
+
             // logado como
-            Text("Logado como ${currentUser.email}"),
+            Text(
+              "Logado como ${currentUser.email}",
+              style: TextStyle(color: Colors.grey),
+            ),
+            const SizedBox(
+              height: 40,
+            )
           ],
         ),
       ),
